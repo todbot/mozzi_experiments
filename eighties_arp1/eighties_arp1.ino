7ddcac00b1c3a1a5d05d29eeda6a3aa0c38f58de
Tod Kurt <todbot@users.noreply.github.com>
2022-01-12 18:37:39 -0800

    moar arp

--- eighties_arp1/eighties_arp1.ino
+++ eighties_arp1/eighties_arp1.ino
@@ -1,34 +1,22 @@
 /**
- * eighties_dystopia_rp2040.ino --
- *  - A swirling ominous wub that evolves over time
- *  - Tested on Raspberry Pi Pico, audio output on pin GP0
- *  - No user input, just wallow in the sound
+ * eighties_arp1.ino --
+ *  - arp arp arp
+ *  - two pots, two buttons, not keys!
  *  
  *  Circuit:
- *  - Copy the PWM cleanup RC filter from schematic figure 3.4.1 "PWM Audio" in
- *     https://datasheets.raspberrypi.com/rp2040/hardware-design-with-rp2040.pdf
- *     also see: https://www.youtube.com/watch?v=rwPTpMuvSXg
- *  - Wire GP0 to input of this circuit, output to TRRS Tip & Ring1
- *  - Become engulfed with dismay in a Cameron-/Carpeter-inspired 1980s dystopia
  *  
  *  Compiling:
- *  - Use the RP2040 Arduino core at: https://github.com/earlephilhower/arduino-pico
- *  - Use the Mozzi fork at https://github.com/pschatzmann/Mozzi
- *  - For slightly better audio quality, add the following after "#define PWM_RATE" in "Mozzi/AudioConfigRP2040.h"
- *      #define AUDIO_BITS 10
- *      #define AUDIO_BIAS ((uint16_t) 512)
- *      #define PWM_RATE (60000*2)
  *  
  *  Code:
- *  - Knob on A0 controls lowpass filter cutoff frequency
- *  - Knob on A1 controls lowpass filter resonance
- *  - Five detuned oscillators are randomly detuned very second or so
- *  - Every 17.5 seconds, a new note is randomly chosen from the allowed note list
+ *  - Knob on A1 controls root note
+ *  - Knob on A2 controls bpm
+ *  - Button on D7 ("RX") controls which arp to play
+ *  - BUtotn on D6 ("TX") controls...
  *  
- * 28 Dec 2021 - @todbot
+ * 12 Jan 2022 - @todbot
  * 
  */
- // Mozzi is very naughty about a few things
+// Mozzi is very naughty about a few things
 //#pragma GCC diagnostic ignored "-Wno-expansion-to-defined"
 
 #include <Adafruit_TinyUSB.h>
@@ -38,6 +26,8 @@
 #include <MozziGuts.h>
 #include <Oscil.h>
 #include <tables/saw_analogue512_int8.h> // oscillator waveform
+#include <tables/square_analogue512_int8.h> // oscillator waveform
+#include <tables/triangle_analogue512_int8.h> // oscillator waveform
 #include <tables/cos2048_int8.h> // filter modulation waveform
 #include <LowPassFilter.h>
 #include <ADSR.h>
@@ -56,34 +46,39 @@ MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDIusb); // USB MIDI
 #define NUM_VOICES 4
 
 Oscil<SAW_ANALOGUE512_NUM_CELLS, AUDIO_RATE> aOscs [NUM_VOICES];
-
 Oscil<COS2048_NUM_CELLS, CONTROL_RATE> kFilterMod(COS2048_DATA);
-
 ADSR <CONTROL_RATE, AUDIO_RATE> envelope;
 LowPassFilter lpf;
-uint8_t cutoff_freq = 100;
-uint8_t resonance = 180; // range 0-255, 255 is most resonant
+
+uint8_t cutoff_freq = 180;
+uint8_t resonance = 100; // range 0-255, 255 is most resonant
 
 int bpm = 130; 
-//uint8_t notes[] = {33, 34, 31}; // possible notes to play MIDI A1, A1#, G1
 
-int8_t arp1[] = {0, 4, 7, 12};   // major
-int8_t arp2[] = {0, 3, 7, 10};   // minor 7th
-int8_t arp3[] = {0, 12, 0, -12}; // octaves
-int8_t arp4[] = {0, 12, 24, -12}; // octaves 2
+const uint8_t arp_len = 4; // length of the arps, all are 4
+int8_t arps[][arp_len] = { 
+  {0, 4, 7, 12},    // major
+  {0, 3, 7, 10},    // minor 7th 
+  {0, 7, 12,0},     // 7 Power 5th
+  {0, 12, 0, -12},  // octaves
+  {0, 12, 24, -12}, // octaves 2
+  {0, -12, -12, 0}, // octaves 3 (bass)
+  {0, 0, 0, 0},     // root
+};
+uint8_t arp_count = 7;
 
-int8_t* arps[] = { arp1, arp2, arp3, arp4 };
-uint8_t arp_count = 4;
-
-uint8_t arp_len = 4; // length of the arps, all are 4
 uint8_t arp_id = 0;  // which arp we using
 uint8_t arp_pos = 0; // where in current arp are we
 
-uint8_t note_base = 40;
+uint8_t root_note = 40;
 uint16_t per_beat_millis = 1000 * 60 / bpm;
 
-//uint16_t note_duration = 200;
+uint8_t note_played = 0;  // the note we have played, so we can unplay it later
+uint16_t note_duration = 0.5 * per_beat_millis;
+
 uint32_t last_beat_millis = 0;
+
+uint8_t patch_num = 0;
 
 Bounce butA = Bounce();
 Bounce butB = Bounce();
@@ -93,10 +88,9 @@ void setup() {
   // Mozzi.setPin(0,2); // this sets RP2040 GP2
   // Mozzi.setPin(0,16); // this sets RP2040 GP16  // MacroPad
   // Mozzi.setPin(0,16); // this sets RP2040 GP16  // Trinkey QT2040 GP16 
-  
-  MIDIusb.begin(MIDI_CHANNEL_OMNI);
+ 
   Serial.begin(115200);
-
+  MIDIusb.begin(MIDI_CHANNEL_OMNI);
   MIDIusb.turnThruOff();    // turn off echo
 
   pinMode( knobAPin, INPUT);
@@ -104,15 +98,9 @@ void setup() {
   butA.attach( butAPin, INPUT_PULLUP);
   butB.attach( butBPin, INPUT_PULLUP);
     
-  delay(3000);
+  //delay(3000);
   startMozzi();
-  kFilterMod.setFreq(0.08f);
-  envelope.setADLevels(255, 10);
-  envelope.setTimes(20, 150, 150, 150 );
-  lpf.setCutoffFreqAndResonance(20, resonance);
-  for( int i=0; i<NUM_VOICES; i++) { 
-     aOscs[i].setTable(SAW_ANALOGUE512_DATA);
-  }
+  setPatch(0);
 }
 
 void loop() {
@@ -131,22 +119,19 @@ void updateControl() {
   bpm = map( knobB, 0,1023, 100, 1600 );
   per_beat_millis =   1000 * 60 / bpm;
   
-  uint8_t note_base_maybe = map( knobA, 0,1023, 24, 72);
+  uint8_t root_note_maybe = map( knobA, 0,1023, 24, 72);
   
   if( butA.fell() ) { // pressed
     Serial.println("next arp");
     arp_id = (arp_id + 1) % arp_count;
   }
   if( butB.fell() ) {
-    //Serial.println("clk div");
-    //clkdiv_id = (clk_id + 1) % arp_count;
+    patch_num = (patch_num+1) % 2;
+    setPatch(patch_num);
   }
   
   // filter range (0-255) corresponds with 0-8191Hz
   // oscillator & mods run from -128 to 127
-  //byte cutoff_freq = 67 + kFilterMod.next()/2;
-  //  byte cutoff_freq = analogRead(A0) / 16; 
-  //  byte resonance = analogRead(A1) / 16; 
   lpf.setCutoffFreqAndResonance(cutoff_freq, resonance);
   envelope.update();
   
@@ -154,22 +139,33 @@ void updateControl() {
   if( millis() - last_beat_millis > per_beat_millis )  {
     last_beat_millis = millis();
     arp_pos = (arp_pos+1) % arp_len;
-    if( arp_pos == 0 ) { // start of a new measure
-      note_base = note_base_maybe;    
+    // only make musical changes at start of a new measure
+    if( arp_pos == 0 ) {
+      root_note = root_note_maybe;
     }
-    noteOn( note_base + arps[arp_id][arp_pos] );
+    note_played = root_note + arps[arp_id][arp_pos];
+    noteOn( note_played );
+  }
+  if( millis() - last_beat_millis > note_duration ) { 
+    noteOff( note_played );
   }
 
 }
 
-void noteOn(byte note) {
+void noteOn(uint8_t note) {
   Serial.print("noteOn:"); Serial.println((byte)note);
   float f = mtof(note);
   for(int i=1;i<NUM_VOICES-1;i++) {
-    aOscs[i].setFreq( f + (float)rand(100)/100); // detune slightly
+    aOscs[i].setFreq( f + (float)rand(150)/100); // detune slightly
+    aOscs[i].setPhase(rand(100)); // helps with clipping on summing if they're not all in phase
   }
-  //aOscs[NUM_VOICES-1].setFreq( (f/2) + (float)rand(100)/1000);
   envelope.noteOn();
+  MIDIusb.sendNoteOn(note, 127, 1); // velocity 127 on chan 1
+}
+
+void noteOff(uint8_t note) {
+  envelope.noteOff();
+  MIDIusb.sendNoteOn(note, 0, 1); // velocity 0 on chan 1
 }
 
 void readMIDI() {
@@ -180,10 +176,30 @@ void readMIDI() {
     byte chan  = MIDIusb.getChannel();
     if( mtype == midi::NoteOn ) {  
       Serial.printf("c:%d t:%2x data:%2x %2x\n", chan, mtype, data1,data2);
+      // we're not using MIDI in yet
     }
-  }  
+  }
 }
-
+void setPatch(uint8_t patchnum) {
+  if( patchnum == 0 ) {
+    for( int i=0; i<NUM_VOICES; i++) { 
+     aOscs[i].setTable(SAW_ANALOGUE512_DATA);
+    }
+    kFilterMod.setFreq(0.08f);
+    envelope.setADLevels(255, 10);
+    envelope.setTimes(20, 200, 200, 200 );
+    lpf.setCutoffFreqAndResonance(cutoff_freq, resonance);
+  }
+  else if( patchnum == 1 ) { 
+    for( int i=0; i<NUM_VOICES; i++) { 
+     aOscs[i].setTable(TRIANGLE_ANALOGUE512_DATA);
+    }
+    kFilterMod.setFreq(0.08f);
+    envelope.setADLevels(255, 10);
+    envelope.setTimes(350, 300, 300, 300);
+    lpf.setCutoffFreqAndResonance(cutoff_freq, resonance);
+  }
+}
 
 // mozzi function, called every AUDIO_RATE to output sample
 AudioOutput_t updateAudio() {
@@ -192,10 +208,6 @@ AudioOutput_t updateAudio() {
     asig += aOscs[i].next();
   }
   asig = lpf.next(asig);
-  return MonoOutput::fromAlmostNBit(18, envelope.next() * asig); // 16 = 8 signal bits + 8 envelope bits                       
+  return MonoOutput::fromNBit(19, envelope.next() * asig); // 16 = 8 signal bits + 8 envelope bits                       
 //  return MonoOutput::fromAlmostNBit(12, asig);
 }
-
-
-//  Serial.printf("butt:%d %d knob:%d %d\n", 
-//    digitalRead(butAPin), digitalRead(butBPin), analogRead(knobAPin), analogRead(knobBPin));
